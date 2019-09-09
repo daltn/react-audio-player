ee87b5c4c7de82010a548bdfb45619c1b1b91a46
dalton.s <dalton.s@iconinteractive.com>
2019-09-09 11:01:16 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./styles.scss";
+
 import AudioPlayer from "./audio-player/AudioPlayer";
+import PageTwo from "./PageTwo";
 
 function App() {
   return (
-    <div className="App">
-      <AudioPlayer />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page-two">Page Two</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={AudioPlayer} />
+        <Route path="/page-two" component={PageTwo} />
+        {/* <AudioPlayer /> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/PageTwo.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PageTwo = () => <h2>test page two</h2>;
+
+export default PageTwo;
